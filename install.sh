27224d221e97c9a449d2b64720331307da3b4c49
Ghazni Nattarshah <ghazni.nattarshah@gmail.com>
2017-04-29 21:12:20 -0400

    Added vim settings.

---
+++ install.sh
@@ -0,0 +1,6 @@
+# go code completion using youcompleteme
+# install CMake and execute below command
+#cd vim/bundle/youcompleteme, hit ENTER
+#./install.py --gocode-completer
+
+vim +PluginInstall +qall
